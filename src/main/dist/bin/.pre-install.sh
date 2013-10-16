3df7bde3ac00509d70320bb2c7ad7678be83709e
Li-na Du <li-na.du@hp.com>
2013-10-16 02:58:26 +0000

    upgrade nexus Professional version to 2.6.3-01
    git-svn-id: file:///opt/mount/casfw/casci-git-migration/casci.svn/source/trunk/nexus-pro-installer@37 f63d2018-cb0f-469b-a947-dce09a3438b4

--- src/main/dist/bin/.pre-install.sh
+++ src/main/dist/bin/.pre-install.sh
@@ -40,7 +40,7 @@ done
 
 # Update Java "cacerts" file with the one that we ship and which contains HP Certificate Authority
 echo "Installing HP Certificate Authority"
-for java_dir in $(ls -d ${CASFW_HOME}/software/oracle-java-1.6.* 2>/dev/null); do
+for java_dir in $(ls -d ${CASFW_HOME}/software/oracle-java-1.7.* 2>/dev/null); do
     cp ${java_dir}/jre/lib/security/cacerts ${java_dir}/jre/lib/security/cacerts.ORIGINAL
     cp ${CASFW_HOME}/etc/security/java6_cacerts ${java_dir}/jre/lib/security/cacerts
 done
