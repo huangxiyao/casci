7fefc50b7e5740a71650ecf59e312fe0cfad8bd9
Li-na Du <li-na.du@hp.com>
2013-10-25 07:44:18 +0000

    find the correct old config.xml
    git-svn-id: file:///opt/mount/casfw/casci-git-migration/casci.svn/source/trunk/build-cloud-installer@54 f63d2018-cb0f-469b-a947-dce09a3438b4

--- build-master-installer/src/main/dist/bin/.pre-config.sh
+++ build-master-installer/src/main/dist/bin/.pre-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CASFW_HOME="$(cd "$(dirname "$0")/.." && pwd -P)"
+BUILD_MASTER_HOME="$(find ${CASFW_HOME} -type d -name "build-master-20*" |tail -n1)"
 #Find old version config.xml file of hudson 
-CONFIG_FILE="$(find ${CASFW_HOME}/../build-master* -type f -name config.xml 2>/dev/null -exec ls -1rt "{}" + | tail -n1)"
+CONFIG_FILE="$(find ${BUILD_MASTER_HOME}/etc/hudson -maxdepth 1 -type f -name config.xml 2>/dev/null -exec ls -1rt "{}" + | head -n1)"
 if [ -n "${CONFIG_FILE}" ]; then
     echo "Find ${CONFIG_FILE} file"
     master_home="$(ls -d ${CASFW_HOME} 2>/dev/null | tail -n1)"
