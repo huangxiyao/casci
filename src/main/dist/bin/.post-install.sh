8186ec9697b219ae01cf4e1301483d30f2dc5c43
Li-na Du <li-na.du@hp.com>
2014-01-10 08:12:07 +0000

    change nexus-webapp-context-path in nexus.properties
    git-svn-id: file:///opt/mount/casfw/casci-git-migration/casci.svn/source/trunk/nexus-installer@72 f63d2018-cb0f-469b-a947-dce09a3438b4

--- src/main/dist/bin/.post-install.sh
+++ src/main/dist/bin/.post-install.sh
@@ -3,7 +3,9 @@ CASFW_HOME="$(cd "$(dirname "$0")/.." && pwd -P)"
 
 NEXUS_PORT=$(grep jetty_nexus_connector_http_port ${CASFW_HOME}/etc/casfw.properties | awk -F "=" '{print $2}')
 
-echo "Please start Nexus with Jetty at http://$(hostname):${NEXUS_PORT}/nexus/"
+NEXUS_PATH=$(grep nexus_webapp_context_path ${CASFW_HOME}/etc/casfw.properties | awk -F "=" '{print $2}')
+
+echo "Please start Nexus with Jetty at http://$(hostname):${NEXUS_PORT}/${NEXUS_PATH}/"
 echo "Using ${CASFW_HOME}/bin/bash nexus.sh start "
 
 echo
