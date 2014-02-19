86d1887858a0f5d0970721ae49198e2c1cea1cea
Li-na Du <li-na.du@hp.com>
2014-02-19 06:19:20 +0000

    change generate command script
    git-svn-id: file:///opt/mount/casfw/casci-git-migration/casci.svn/source/trunk@101 f63d2018-cb0f-469b-a947-dce09a3438b4

--- scripts/generate-command.sh
+++ scripts/generate-command.sh
@@ -13,9 +13,9 @@ fi
 report_file=$1
 command_file=$2
 
-repositoryId="$(awk -F $'\t' '{print $2}' ${report_file} | sed 's!.*storage/!!' | awk -F $'/' '{print $1}' |sort| uniq )"
+repositoryId="$(awk -F $'\t' '{print $2}' ${report_file} | sed 's!.*storage/!!' | awk -F $'/' '{print $1}' |sort| uniq |sed '/^$/d')"
 
-if [ "${repositoryId}"x == "releases"x -o "${repositoryId}"x == "snapshots"x ] ; then
+if [ "${repositoryId}"x = "releases"x -o "${repositoryId}"x = "snapshots"x ] ; then
 	echo "delete artifacts on nexus releases or snapshots repository ."
 else 
 	echo "please enter the correct report file ."
@@ -27,7 +27,7 @@ if [ "$3"x = "itg"x ]; then
 	awk -F $'\t' '$4 ~ /X/ {print $2}' ${report_file} |sed "s!/storage/${repositoryId}/!/storage/${repositoryId}/content/!" | sed 's!.*storage/!curl -X DELETE -u admin:casiscool http://gvt1344.austin.hp.com/nexus/service/local/repositories/!' > ${command_file}
 elif [ "$3"x = "pro"x ]; then
 	#for PRO
-	awk -F $'\t' '$4 ~ /X/ {print $2}' ${report_file} |sed "/storage/${repositoryId}/!/storage/${repositoryId}/content/!" | sed 's!.*storage/!curl -X DELETE -u admin:casiscool http://repo1.corp.hp.com/nexus/service/local/repositories/!' > ${command_file}
+	awk -F $'\t' '$4 ~ /X/ {print $2}' ${report_file} |sed "s!/storage/${repositoryId}/!/storage/${repositoryId}/content/!" | sed 's!.*storage/!curl -X DELETE -u admin:casiscool http://repo1.corp.hp.com/nexus/service/local/repositories/!' > ${command_file}
 else 
 	echo "please enter the correct environment."
 	exit 1;
