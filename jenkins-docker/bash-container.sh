e72903936882ccce29dc0f5126c4aa5453bfecd1
Quintin May <quintin.may@hp.com>
2015-07-16 15:55:43 -0400

    Manage all secure files in /home/jenkins
    Base data container on casci/jenkins rather than casci/jenkins-cas for /home/jenkins VOLUME

--- jenkins-docker/bash-container.sh
+++ jenkins-docker/bash-container.sh
@@ -3,11 +3,11 @@
 docker ps -a | grep hpq-data || docker create --name hpq-data casci/jenkins-data-hpq
 
 docker run --rm --interactive --tty \
+           --read-only \
            --publish 8080:8080 \
            --publish 8443:8443 \
            --volumes-from hpq-data \
            --volume ~/Library/Caches/org.apache.maven/repository:/var/opt/jenkins/maven-repository \
            casci/jenkins-cas bash
 
-#           --read-only \
 #           --env HTTP_PROXY="http://web-proxy.corp.hp.com:8080" \
