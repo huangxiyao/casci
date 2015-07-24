faf10edc48c77824f085f275af505023470ef536
Quintin May <quintin.may@hp.com>
2015-07-24 10:28:11 -0400

    Make inbound SSL work
    Don't mount casfw Maven repository

--- jenkins-docker/hp-bash-container.sh
+++ jenkins-docker/hp-bash-container.sh
@@ -6,7 +6,8 @@ docker run --rm --interactive --tty \
            --read-only \
            --publish 9080:8080 \
            --publish 9443:8443 \
-           --env HTTP_PROXY=http://web-proxy.corp.hpcom:8080 \
+           --env HTTP_PROXY=http://web-proxy.corp.hp.com:8080 \
            --volumes-from hpq-data \
-           --volume /opt/casfw/var/maven-repository:/var/opt/jenkins/maven-repository \
            casci/jenkins-cas bash
+
+#            --volume /opt/casfw/var/maven-repository:/var/opt/jenkins/maven-repository \
