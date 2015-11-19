069c4c50f19771916b2c469234e223970bf15ee4
Zhi-Zhi Cai <zhi-zhi.cai@hp.com>
2015-11-19 23:14:42 +0800

    Variable hudson_master_url  ==> hudson_master
    This variable will conflict with the one in casfw.properties which is also named hudson_master_url.

--- ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
+++ ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
@@ -9,7 +9,7 @@ build_slave_cdi="build-slave-installer-${build_release_version}.cdi"
 host_name=$(hostname)
 link="ci"
 pid_file="${casfw_home}/${link}/var/hudson-slave.pid" 
-hudson_master_url="c4t17569.itcs.hpecorp.net:18780"
+hudson_master="c4t17569.itcs.hpecorp.net:18780"
 
 function checkHudsonInstallation {
     if [ -d ${casfw_home}/${build_slave_dir} ]; then
@@ -53,7 +53,7 @@ function downloadCdiInstall {
 function configureHudson {
     ln -sf ${casfw_home}/${build_slave_dir}/ ${casfw_home}/${link}
     cd ${casfw_home}/${link}/etc
-    sed -i 's/gvt1344.austin.hp.com/${hudson_master_url}/g' casfw.properties.itg
+    sed -i 's/gvt1344.austin.hp.com/${hudson_master}/g' casfw.properties.itg
     sed -i 's/g1t1044g.austin.hp.com/${host_name}/g' casfw.properties.itg
     bash ${casfw_home}/${link}/bin/config.sh -e itg
 }
