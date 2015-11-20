05c8ec9e5af79438b521a3bfbf38e70000491813
Zhi-Zhi Cai <zhi-zhi.cai@hp.com>
2015-11-20 15:59:07 +0800

    Use " " instead of ' ' in sed command

--- ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
+++ ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
@@ -53,8 +53,8 @@ function downloadCdiInstall {
 function configureHudson {
     ln -sf ${casfw_home}/${build_slave_dir}/ ${casfw_home}/${link}
     cd ${casfw_home}/${link}/etc
-    sed -i 's/gvt1344.austin.hp.com/${hudson_master}/g' casfw.properties.itg
-    sed -i 's/g1t1044g.austin.hp.com/${host_name}/g' casfw.properties.itg
+    sed -i "s/gvt1344.austin.hp.com/${hudson_master}/g" casfw.properties.itg
+    sed -i "s/g1t1044g.austin.hp.com/${host_name}/g" casfw.properties.itg
     bash ${casfw_home}/${link}/bin/config.sh -e itg
 }
 
