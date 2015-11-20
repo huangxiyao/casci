a89bb91644ecf05a1ace6354566ea0c47e101d13
Zhi-Zhi Cai <zhi-zhi.cai@hp.com>
2015-11-20 16:51:10 +0800

    modification of codes in shell scripts

--- ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
+++ ansible-hudson-nexus/roles/hudson-slave/files/hudson-slave-install.sh
@@ -51,8 +51,9 @@ function downloadCdiInstall {
 }
 
 function configureHudson {
-    ln -sf ${casfw_home}/${build_slave_dir}/ ${casfw_home}/${link}
-    cd ${casfw_home}/${link}/etc
+    cd ${casfw_home}
+    ln -sf ${build_slave_dir}/ ${link}
+    cd ${link}/etc
     sed -i "s/gvt1344.austin.hp.com/${hudson_master}/g" casfw.properties.itg
     sed -i "s/g1t1044g.austin.hp.com/${host_name}/g" casfw.properties.itg
     bash ${casfw_home}/${link}/bin/config.sh -e itg
