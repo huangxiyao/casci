1c9a31894f66e05f71313397df87d2683c4a3a8c
Zhi-Zhi Cai <zhi-zhi.cai@hp.com>
2015-12-04 16:39:46 +0800

    add umask and switch current user to casfw

--- build-cloud-installer/build-master-installer/src/main/dist/bin/tomcat-sonar.sh
+++ build-cloud-installer/build-master-installer/src/main/dist/bin/tomcat-sonar.sh
@@ -2,6 +2,12 @@
 
 # This file is deployed into {casfw_home}/bin so let's walk up
 # the directory hierarchy to get to CASFW root directory
+umask u=rwx,g=rx,o=rx
+
+if [ $(id -un) != "casfw" ]; then
+    exec su -m "${USER}" -c "$0 $@"
+fi
+
 CASFW_HOME="$(cd "$(dirname "$0")/.." && pwd -P)"
 
 source ${CASFW_HOME}/bin/.casfwrc
