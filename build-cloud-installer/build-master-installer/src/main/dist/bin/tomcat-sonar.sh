b067e4cd02826b1f06fbd7aa52208341cf29bd1d
Cai <zhi-zhi.cai@hpe.com>
2015-12-11 11:20:49 +0800

    ajust umask position

--- build-cloud-installer/build-master-installer/src/main/dist/bin/tomcat-sonar.sh
+++ build-cloud-installer/build-master-installer/src/main/dist/bin/tomcat-sonar.sh
@@ -3,11 +3,12 @@
 # This file is deployed into {casfw_home}/bin so let's walk up
 # the directory hierarchy to get to CASFW root directory
 USER="casfw"
-umask u=rwx,g=rx,o=rx
 
 if [ $(id -un) != "casfw" ]; then
     exec su -m "${USER}" -c "$0 $@"
 fi
+
+umask u=rwx,g=rx,o=rx
 
 CASFW_HOME="$(cd "$(dirname "$0")/.." && pwd -P)"
 
