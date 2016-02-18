c14918167dc664868473299c8e8f17990585ec05
Zhi-Zhi Cai <zhi-zhi.cai@hp.com>
2016-02-18 22:29:54 +0800

    Revert "update last merge and add yum config"

--- nexus-installer/src/main/dist/bin/.post-config.sh
+++ nexus-installer/src/main/dist/bin/.post-config.sh
@@ -105,8 +105,3 @@ else
 fi
 echo "spliting nexus data directory successfully"
 
-# Automate YUM configuration
-if [ -f "${CASFW_HOME}/etc/capabilities_original.xml" ]; then
-    cp ${CASFW_HOME}/etc/capabilities_original.xml ${CASFW_HOME}/software/sonatype-work/conf/capabilities.xml
-    echo "Configured Capabilities."
-fi
