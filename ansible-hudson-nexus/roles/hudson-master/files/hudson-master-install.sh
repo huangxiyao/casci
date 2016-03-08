406ac3c458fd839ad7854a0f9ece36776a845a28
Cai <zhi-zhi.cai@hpe.com>
2016-03-08 11:33:21 +0800

    playbook update

--- ansible-hudson-nexus/roles/hudson-master/files/hudson-master-install.sh
+++ ansible-hudson-nexus/roles/hudson-master/files/hudson-master-install.sh
@@ -9,22 +9,32 @@ hudson_master_dir="build-master-${hudson_master_release_version}"
 hudson_master_cdi="build-master-installer-${hudson_master_release_version}.cdi"
 host_name=$(hostname)
 link="ci"
-pid_file="${casfw_home}/${link}/var/tomcat-hudson.pid" 
+hudson_pid="${casfw_home}/${link}/var/tomcat-hudson.pid"
+sonar_pid="${casfw_home}/${link}/var/tomcat-soanr.pid" 
 
 function checkHudsonInstallation {
     if [ -d "${casfw_home}/${hudson_master_dir}" ]; then
-      echo -ne "YES"
+        echo -ne "YES"
     else 
       echo -ne "NO"
     fi
 }
 
 function checkHudsonProgress {
-    if [ -f "${pid_file}" ]; then
- 	  s=$(printf " %s " $(ps -e | grep $(cat "${pid_file}")) | awk '{ print $1 }');
- 	  if [ -n "$s" ]; then
- 	    echo -ne "Running";
- 	  fi; 
+    if [ -f "${hudson_pid}" ]; then
+      s=$(printf " %s " $(ps -e | grep $(cat "${hudson_pid}")) | awk '{ print $1 }');
+      if [ -n "$s" ]; then
+        echo -ne "Running";
+      fi;
+    fi
+}
+
+function checkSonarProgress {
+    if [ -f "${sonar_pid}" ]; then
+      s=$(printf " %s " $(ps -e | grep $(cat "${sonar_pid}")) | awk '{ print $1 }');
+      if [ -n "$s" ]; then
+        echo -ne "Running";
+      fi;
     fi
 }
 
@@ -39,8 +49,13 @@ function prepareInstallation {
     checkHudsonProgress
     if [[ $? -eq Running ]]; then
       bash "${casfw_home}/${link}/bin/tomcat-hudson.sh" stop
+      echo -ne "Current Hudson master has stopped"
     fi
-    echo -ne "Current Hudson master has stopped"
+    checkSonarProgress
+    if [[ $? -eq Running ]]; then
+      bash "${casfw_home}/${link}/bin/tomcat-sonar.sh" stop
+      echo -ne "Current Sonar has stopped"
+    fi
 }
 
 function downloadCdiInstall {
@@ -55,16 +70,17 @@ function configureHudson {
     ln -sf "${hudson_master_dir}/" "${link}"
     cd "${link}/etc"
     if [[ ${environment} == "pro" ]]; then
-        sed -i "s/build1.core.hpecorp.net/${host_name}:18780/g" casfw.properties.pro
+        sed -i "s/build1.core.hpecorp.net/${host_name}/g" casfw.properties.pro
         bash "${casfw_home}/${link}/bin/config.sh" -e pro
     else
-        sed -i "s/build1-itg.core.hpecorp.net/${host_name}:18780/g" casfw.properties.itg
+        sed -i "s/build1-itg.core.hpecorp.net/${host_name}/g" casfw.properties.itg
         bash "${casfw_home}/${link}/bin/config.sh" -e itg
     fi
 }
 
 function startHudson {
     bash "${casfw_home}/${link}/bin/tomcat-hudson.sh" start
+    bash "${casfw_home}/${link}/bin/tomcat-sonar.sh" start
 }
 
 $function_name
